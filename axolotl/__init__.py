4d8edbf4f3ad78c0dbbd2daefd611d962377af37
FVortex <hty16@mail.ustc.edu.cn>
2024-08-30 20:26:53 +0800

    限制 import

--- axolotl/__init__.py
+++ axolotl/__init__.py
@@ -5,8 +5,10 @@ from axolotl.instrument import InstrumentManager
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
 handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter('[%(asctime)s][%(name)s][%(levelname)s] %(message)s'))
+handler.setFormatter(logging.Formatter('[%(asctime)s][%(name)s][%(levelname)s]: %(message)s'))
 logger.addHandler(handler)
 
 InstrumentManager.CHANNEL_EVENT_BUS.mute_logger(True)
+
+__all__ = ['InstrumentManager']
 
